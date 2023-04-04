e052947007274e8ac6ffe483deac881d1e520e9e
Luis Capote <lcapote@opensistemas.com>
2023-04-04 10:41:08 -0500

    Update urls.py

--- bigbluebuttonapp/urls.py
+++ bigbluebuttonapp/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
+from django.urls import path, include, re_path
 from django.contrib.auth.decorators import login_required
 
 from .views import bigbluebuttonapp_view
 
-urlpatterns = [
-    url(r"^/$", bigbluebuttonapp_view , name="bigbluebuttonapp_view")
-]
+# urlpatterns = [
+#     url(r"^/$", bigbluebuttonapp_view , name="bigbluebuttonapp_view")
+# ]
 
+
+urlpatterns = [
+    path(r"^/$", bigbluebuttonapp_view , name="bigbluebuttonapp_view")
+]
